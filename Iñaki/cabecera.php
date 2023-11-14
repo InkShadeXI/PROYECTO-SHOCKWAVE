878f0bf79e11d34e11ab2d3d93acdd03c261fbcf
Ignacio Milán <145646795+TyrionMilan@users.noreply.github.com>
2023-11-14 12:49:04 +0100

    Create cabecera.php

---
+++ Iñaki/cabecera.php
@@ -0,0 +1,84 @@
+<!-- Enlazar en el resto de php con require -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  margin-left: 8px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+    background-color: #111;
+}
+
+p{
+    text-align: center;
+    color: white;
+}
+
+li a:hover:active {
+  background-color: #04AA6D;
+}
+body{
+  background-image: url("logo.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-attachment: fixed;
+}
+#buscador{
+  background-color: gainsboro;
+  margin-top: 13px;
+  border-radius: 5px;
+}
+</style>
+</head>
+<header>
+    <?php
+    session_start();
+if(!isset($_SESSION['usuario'])){
+  header("Location: login.php?redirigido=true");
+    }else if($_SESSION['usuario']['usuario_admin'] == 1){
+    echo "
+    <ul>
+    <li><p>Usuario:  ".$_SESSION['usuario']['nombre_usuario']."</p></li>
+    <li><a href=home_shockwave.php>Home</a></li>
+    <li><a href=perfil.php>Perfil</a> </li>
+    <li><a href=zonaadmin.php>Zona de administrador</a></li>
+    <li><input type='text' id='buscador' placeholder='Indica usuario a buscar...'></li<>
+    <li><a href=logout.php>Cerrar sesión</a></li>
+    </ul>";
+        }else if($_SESSION['usuario']['usuario_admin'] == 0){
+            echo "
+            <ul>
+            <li><p>Usuario:  ".$_SESSION['usuario']['nombre_usuario']."</p></li>
+            <li><a href=home_shockwave.php>Home</a></li>
+            <li><a href=perdil.php>Perfil</a> </li>
+            <li><input type='text' id='buscador' placeholder='Indica usuario a buscar...'></li<>
+            <li><a href=logout.php>Cerrar sesión</a></li>
+            </ul>";
+        }
+    ?>
+ 
+</header>
+<body>
+
+</body>
+</html>
