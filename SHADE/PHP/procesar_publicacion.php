d8d5e9e0f940b5def3278ae02c2238824c045c4a
InkShadeXI <145647024+InkShadeXI@users.noreply.github.com>
2023-11-20 12:40:55 +0100

    Update procesar_publicacion.php

--- SHADE/PHP/procesar_publicacion.php
+++ SHADE/PHP/procesar_publicacion.php
@@ -22,22 +22,50 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
                 // El usuario que buscamos
                 $seleccion_usuario = $_SESSION['usuario']['nombre_usuario']; 
-                $id_usuario = "SELECT ID_USUARIO FROM USUARIO WHERE NOMBRE_USUARIO = $seleccion_usuario";
-                $texto_publicacion = $_POST['texto_publicacion'];
-                if (isset($_POST['foto_publicacion'])) {
-                    $existe_foto = true;
-                    $foto = $_POST['foto_publicacion'];
-                }
+                $id_usuario = $bd->prepare("SELECT ID_USUARIO FROM USUARIO WHERE NOMBRE_USUARIO = ?");
+                $id_usuario->execute(array($seleccion_usuario));
+
+                // Obtener el resultado de la consulta
+                $id_usuario_resultado = $id_usuario->fetch(PDO::FETCH_ASSOC);
+
+                if ($id_usuario_resultado) {
+                    $id_usuario = $id_usuario_resultado['ID_USUARIO'];
+                } 
                 else {
+                    echo "Error: Usuario no encontrado.";
+                }
+
+                $texto_publicacion = $_POST['texto_publicacion'];
+                if (isset($_FILES['foto_publicacion']) && $_FILES['foto_publicacion']['error'] == 0) {
+                    $existe_foto = true;
+                    $foto = $_FILES['foto_publicacion']['name'];
+                
+                    // Directorio donde se guardará la foto
+                    $nombre_usuario = $_SESSION['usuario']['nombre_usuario'];
+                    $directorio_destino = "IMG/USUARIOS/{$nombre_usuario}/";
+                
+                    // Verificar si el directorio existe, si no, crearlo
+                    if (!file_exists($directorio_destino)) {
+                        mkdir($directorio_destino, 0777, true);
+                    }
+                
+                    // Ruta completa para guardar la foto en el directorio destino
+                    $ruta_destino = $directorio_destino . $foto;
+                
+                    // Mover la foto al directorio destino
+                    move_uploaded_file($_FILES['foto_publicacion']['tmp_name'], $ruta_destino);
+                
+                } else {
                     $existe_foto = false;
                     $foto = null;
                 }
-                $cracion_publicacion = "INSERT INTO POST_USUARIO VALUES ($id_usuario, '$texto_publicacion', $existe_foto, '$foto', 0, 0)";
-                
-
+                $cracion_publicacion = $bd->prepare("INSERT INTO POST_USUARIO (ID_USUARIO_POST, TEXTO_POST, EXISTE_FOTO, TITULO_FOTO, NUM_LIKES, NUM_DISSLIKES) 
+                VALUES (?, ?, ?, ?, 0, 0)");
+                $cracion_publicacion->execute(array($id_usuario, $texto_publicacion, $existe_foto, $foto));
+                header("Location: home_shockwave.php");
             } 
             catch (PDOException $e) {
-                echo "Error: " . $e->getMessage();
+                echo "Error: " . $e->getMessage();                                                 
             }  
         }
     }
