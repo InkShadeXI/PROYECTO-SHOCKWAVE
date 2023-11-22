63933ebb3615edacec92eb1f1b7cbf72be2b22e8
Ignacio Milán <145646795+TyrionMilan@users.noreply.github.com>
2023-11-22 12:05:36 +0100

    Update usuario.php

--- Iñaki/usuario.php
+++ Iñaki/usuario.php
@@ -8,38 +8,47 @@
 <body>
     <?php
         // Comprobar si hay búsqueda de usuarios si no mostrar el listado completo
-        function buscarUsuario(){    
-            //   $search = $_POST['busqueda'];
-               $cadena_conexion = 'mysql:dbname=redsocial;host=127.0.0.1';
-               $base = 'root';
-               $clave = '';
-               $bd = new PDO($cadena_conexion, $base, $clave);
-               $ins = "select nombre_usuario from usuario where nombre_usuario LIKE '%U%'";
-           
-               $resul = $bd->query($ins);
-               if($resul->rowCount() >= 1){		
-                   return $resul->fetch();		
-               }else{
-                   return FALSE;
+        function buscarUsuario() {
+            if(isset( $_POST['buscador'])){
+                $search = $_POST['buscador'];
+                if(empty( $_POST['buscador'])){
+                
+                } else{
+                $cadena_conexion = 'mysql:dbname=redsocial;host=127.0.0.1';
+                $base = 'root';
+                $clave = '';
+                $bd = new PDO($cadena_conexion, $base, $clave);
+            
+                  
+              
+                    $stmt = $bd->prepare("SELECT nombre_usuario  FROM usuario WHERE nombre_usuario LIKE :search OR correo_usuario = :searchcorr");
+                    $stmt->bindValue(':search', '%' . $search . '%');
+                    $stmt->bindValue(':searchcorr', $search);
+                    $stmt->execute();
+            
+                    if ($stmt->rowCount() >= 1) {
+                        return $stmt->fetchAll();
+                    } else {
+                        return FALSE;
+                     }
+                  }
                }
-           }
+        }
 
 
             // En caso de encontrar que liste usuarios    
            try{
-           $resultBusqueda = buscarUsuario();
-           if (count($resultBusqueda) > 0) {
-               foreach ($resultBusqueda as $usuario) {
-                  
-                   if($usuario != $usuarioanterior){
-                       $usuarioanterior = $usuario;
-                       echo $usuario . "<br>";
-                   }else{
-                       echo"se repite usuario";
-                   }
-               }
-           } else {
-               echo "No users found.";
+           $resultBusqueda = buscarUsuario($_POST["buscador"]);
+           if ($resultBusqueda == false) {
+                echo "<div id='vacio'>No hay  usuarios</div>";
+           } else if(count($resultBusqueda) <= 0){
+              echo "no existen usuarios";
+               }else if(count($resultBusqueda) > 0){
+                echo "<div id='contenedor'>";
+                   foreach ($resultBusqueda as $usuario) {
+                      echo  "<div class='perfiles'><a href=perfil.php?usu=".$usuario['nombre_usuario'].">".$usuario['nombre_usuario']."</div>"; 
+                    }
+                echo "</div>";    
            }
            }catch (PDOException $e) {
                echo 'Error con la base de datos: ' . $e->getMessage();
